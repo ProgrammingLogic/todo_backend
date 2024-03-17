980a68489b4d5621257dd77aa7818195775ef5c1
Jonathyn Stiverson <43549238+ProgrammingLogic@users.noreply.github.com>
2024-03-17 14:06:09 -0500

    Add dueDate and createdDate to TodoItem.cs

--- models/TodoItem.cs
+++ models/TodoItem.cs
@@ -4,4 +4,6 @@ public class TodoItem {
     public long Id {get; set;}
     public string? Name {get; set;}
     public bool isComplete {get; set;}
+    public DateTime dueDate {get; set;}
+    public DateTime createdDate {get; set;}
 }
